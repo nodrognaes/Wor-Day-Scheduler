da2b64e219d057f3fa44d64fa11ba3ac34b569de
Sean Gordon <nodrog.naes@gmail.com>
2021-05-15 23:19:16 -0400

    js updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,34 @@ var currentDayEl = $('#currentDay');
 var savedMsgEl = $('#saved-message');
 var saveBtn = $('#saveBtn');
 
-function displayTime() {
+//current date in header
+function displayDate() {
     var currentDay = moment().format('dddd, MMM DD, YYYY');
     currentDayEl.text(currentDay);
   }
 
-  displayTime();
+  displayDate();
 
-
+//flash "Saved!" when saved button pressed - code incorrect
 saveBtn.addEventListener("click", saved);
 
  function saved() {
-   savedMsgEl.setAttribute("class", "");
+   savedMsgEl.setAttribute("class", "show");
   setTimeout(function() {
     savedMsgEl.setAttribute("class", "hide");;
   }, 1000);
 }
+
+/*
+//if current hour < time , change bg to class "past"
+if ( < ) {
+
+}
+//if time === current hour, change bg to class "present"
+if ( === ) {
+
+}
+//if current hr > time change bg to class "future"
+if ( > ) {
+
+} */
